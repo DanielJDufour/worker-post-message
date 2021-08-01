780923ef0a8e46b40dd6f31facfdbb701d000b34
DanielJDufour <daniel.j.dufour@gmail.com>
2021-08-01 10:35:05 -0400

    added Buffer support

--- worker-post-message.js
+++ worker-post-message.js
@@ -1,6 +1,24 @@
+const isTransferable = data => {
+  try {
+    if (data instanceof ArrayBuffer) {
+      return true;
+    }
+  } catch (error) {
+    // pass
+  }
+  
+  try {
+    if (typeof Buffer !== "undefined" && Buffer.isBuffer(data)) {
+      return true;
+    }
+  } catch (error) {
+    // pass
+  }
+};
+
 const getTransferables = ({ debug, inpt }) => {
   const results = [];
-  if (inpt instanceof ArrayBuffer) results.push(inpt);
+  if (isTransferable(inpt)) results.push(inpt);
   else if (Array.isArray(inpt)) results.push(...inpt.map(getTransferables).flat());
   else if (typeof inpt === "object") results.push(...Object.values(inpt).map(getTransferables).flat());
   return results;
